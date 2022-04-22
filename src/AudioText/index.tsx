1fd7db5d59fdfe1b4d20e5449a4e7ebb21888d0f
Jacob Tingling <jtingling@live.com>
2022-04-22 16:59:13 -0400

    Words render one at a time.

--- src/AudioText/index.tsx
+++ src/AudioText/index.tsx
@@ -1,17 +1,40 @@
-import {Sequence, useVideoConfig} from 'remotion';
+import {
+	interpolate,
+	useCurrentFrame,
+	useVideoConfig,
+	getInputProps,
+} from 'remotion';
+import {Container, Paper, Typography} from '@mui/material';
+import {useEffect, useCallback, useState} from 'react';
+
+const title = getInputProps();
+
 export const AudioText: React.FC<{
 	titleText: string;
 }> = ({titleText}) => {
+	const [wordArray, setWordArray] = useState<string[]>([]);
+	const frame = useCurrentFrame();
+
+	const populatePassage = useCallback(() => {
+		const tempArray = wordArray;
+		tempArray.push(titleText);
+		tempArray.push(' ');
+		if (tempArray.length < 150) {
+			setWordArray(tempArray);
+		} else {
+			setWordArray([]);
+		}
+	}, [titleText, wordArray]);
+
+	useEffect(() => {
+		populatePassage();
+	}, [populatePassage]);
+
 	return (
-		<div
-			style={{
-				width: '50%',
-				height: '180px',
-				zIndex: 5,
-				justifyContent: 'center',
-			}}
-		>
-			{titleText}
-		</div>
+		<Container sx={{zIndex: 10, whiteSpace: 'pre-line'}}>
+			<Paper>
+				<Typography variant="h6">{wordArray}</Typography>
+			</Paper>
+		</Container>
 	);
 };
